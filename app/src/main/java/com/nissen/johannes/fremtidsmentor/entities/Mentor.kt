2e09ce2a832e6121bb78459e34e5e2b4122cb13f
Johannes Nissen <32563468+JoeInator@users.noreply.github.com>
2019-10-11 11:38:41 +0200

    Updated properties of Mentors

--- app/src/main/java/com/nissen/johannes/fremtidsmentor/entities/Mentor.kt
+++ app/src/main/java/com/nissen/johannes/fremtidsmentor/entities/Mentor.kt
@@ -1,11 +1,22 @@
 package com.nissen.johannes.fremtidsmentor.entities
 
-class Mentor(id: String, username: String, password: String,descr: String) : User(id, username, password) {
+class Mentor() : User() {
 
+    private var teaser: String? = null
     private var description: String? = null
     private var id: String? = null
     private var username: String? = null
     private var password: String? = null
+    private var competencies: ArrayList<String>? = null
+    
+    constructor(id: String, username: String, password: String, teaser: String, descr: String, competencies: ArrayList<String>) : this() {
+        this.id = id
+        this.username = username
+        this.password = password
+        this.teaser = teaser
+        this.description = descr
+        this.competencies = competencies
+    }
 
     fun getDescription(): String? {
         return description
@@ -39,4 +50,31 @@ class Mentor(id: String, username: String, password: String,descr: String) : Use
         this.password = password
     }
 
+    fun getTeaser(): String?{
+        return teaser
+    }
+
+    fun setTeaser(teaser: String) {
+        this.teaser = teaser
+    }
+
+    fun getComps(): ArrayList<String>? {
+        return competencies
+    }
+
+    fun setComps(comps: ArrayList<String>) {
+        this.competencies = comps
+    }
+
+    fun addToComps(newComp: String) {
+        if (!competencies!!.contains(newComp))
+            competencies!!.add(newComp)
+    }
+
+    fun RemoveComps(comp: String) {
+        if(competencies!!.contains(comp))
+            competencies!!.remove(comp)
+    }
+
+    
 }
