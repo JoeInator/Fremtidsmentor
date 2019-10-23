549028c40198a59baa7a610ffd741dbd58f9d2dd
Johannes Nissen <nissen.johannes@gmail.com>
2019-10-23 11:50:30 +0200

    Failsafe for firebase call

--- app/src/main/java/com/nissen/johannes/fremtidsmentor/screenshots/FragmentChosenMentor.kt
+++ app/src/main/java/com/nissen/johannes/fremtidsmentor/screenshots/FragmentChosenMentor.kt
@@ -1,12 +1,17 @@
 package com.nissen.johannes.fremtidsmentor.screenshots
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.database.*
 import com.nissen.johannes.fremtidsmentor.R
+import com.nissen.johannes.fremtidsmentor.entities.Mentor
 import kotlinx.android.synthetic.main.fragment_chosen_mentor.*
 import kotlinx.android.synthetic.main.fragment_chosen_mentor.view.*
 import java.lang.StringBuilder
@@ -15,33 +20,24 @@ class FragmentChosenMentor: Fragment() {
 
     private val compList1: ArrayList<String> = arrayListOf<String>()
     private val compList2: ArrayList<String> = arrayListOf<String>()
+    lateinit var builder: StringBuilder
+    lateinit var mPrefs: SharedPreferences
+    lateinit var prefsEditor: SharedPreferences.Editor
+    lateinit var ref: DatabaseReference
+    lateinit var mentor: Mentor
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater.inflate(R.layout.fragment_chosen_mentor, container, false)
 
-        view.mentors_pic.setImageResource(R.drawable.cv_foto)
-        val comps: ArrayList<String> = arguments!!.getStringArrayList("competencies")
-        val builder: StringBuilder = StringBuilder()
-        builder.append("")
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        prefsEditor = mPrefs.edit()
+        ref = FirebaseDatabase.getInstance().getReference("users/mentor")
 
-        FillLists(comps)
+        getMentorFromFirebase(arguments!!.getString("name"))
+        Handler().postDelayed({
+            loadPage(view)
+        }, 1000)
 
-        for (i in 0 until compList1.size) {
-            builder.append(compList1[i]+"\n")
-        }
-        view.InfoView.vertical_scroll.horisontal_scroll.comp_list1.text = builder.toString()
-
-        for (i in 0 until compList2.size) {
-            builder.append(compList2[i]+"\n")
-        }
-        view.InfoView.vertical_scroll.horisontal_scroll.comp_list2.text = builder.toString()
-
-        view.InfoView.vertical_scroll.mentor_description.text = arguments!!.getString("description")
-        view.mentor_basicinfo.text = arguments!!.getString("name")
-
-        view.bookingBtn.setOnClickListener{
-            Toast.makeText(this.requireContext(), R.string.Not_Implemented, Toast.LENGTH_SHORT).show()
-        }
 
         return view
     }
@@ -59,4 +55,51 @@ class FragmentChosenMentor: Fragment() {
             compList2.add(comps[i])
         }
     }
+
+    private fun loadPage(view: View) {
+        view.mentors_pic.setImageResource(R.drawable.cv_foto)
+        val comps: ArrayList<String> = mentor.getComps()!!
+        builder = StringBuilder()
+        builder.append("")
+
+        FillLists(comps)
+
+        for (i in 0 until compList1.size) {
+            builder.append(compList1[i]+"\n")
+        }
+        view.InfoView.vertical_scroll.horisontal_scroll.comp_list1.text = builder.toString()
+
+        for (i in 0 until compList2.size) {
+            builder.append(compList2[i]+"\n")
+        }
+        view.InfoView.vertical_scroll.horisontal_scroll.comp_list2.text = builder.toString()
+
+        view.InfoView.vertical_scroll.mentor_description.text = mentor.getDescription()
+        view.mentor_basicinfo.text = mentor.getTeaser()
+
+        view.bookingBtn.setOnClickListener{
+            Toast.makeText(this.requireContext(), R.string.calendarView_under_construction, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    /* Retrive the mentor from firebase */
+    private fun getMentorFromFirebase(mentorName: String) {
+
+        ref.addValueEventListener(object: ValueEventListener  {
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(requireContext(), R.string.firebaseError, Toast.LENGTH_SHORT).show()
+                activity!!.supportFragmentManager.popBackStack()
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                for (m in p0.children) {
+                    if (m.child("username").getValue(String::class.java).equals(mentorName)) {
+                        mentor = m.getValue(Mentor::class.java)!!
+                    }
+                }
+            }
+        })
+    }
+
+
 }
